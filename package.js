0c5dfb0b361f5cc17a35db999c4cac04a687c739
sam@hatoum.net <sam@hatoum.net>
2015-02-19 10:44:11 -0800

    fixed dependency issue

--- package.js
+++ package.js
@@ -7,7 +7,8 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
-  api.use(['xolvio:http-interceptor@0.2.0'], ['server']);
+  api.use('github', ['client', 'server']);
+  api.use(['xolvio:http-interceptor@0.4.0'], ['server']);
   api.use(['iron:router@1.0.6'], ['server']);
   api.add_files('oauth-fake-client.js', 'client');
   api.add_files('github-fake.js', ['server']);
